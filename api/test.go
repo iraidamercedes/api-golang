112e21a0a7d38c865c414a1c0d24e28e9921d5d7
iraida07 <iraidabarreto07@hotmail.com>
2020-06-18 03:11:00 -0500

    :art: Take values and creates json

--- api/test.go
+++ api/test.go
@@ -16,9 +16,9 @@ type endpoint struct {
 }
 
 type Server struct {
-	Host      string
-	Status    string
-	Endpoints []endpoint
+	Endpoints []endpoint `json:"servers"`
+	Host      string     `json:"Host"`
+	Status    string     `json:"Status"`
 }
 
 func requestURL(ctx *fasthttp.RequestCtx) {
@@ -29,13 +29,27 @@ func requestURL(ctx *fasthttp.RequestCtx) {
 	resp, _ := client.Do(req)
 	servers, _ := ioutil.ReadAll(resp.Body)
 	//println(string(servers))
-	fmt.Fprintf(ctx, "%s", string(servers))
+	//fmt.Fprintf(ctx, "%s", string(servers))
 
 	var info Server
 	if err := json.Unmarshal([]byte(servers), &info); err != nil {
 		log.Fatal("Failed to generate json", err)
 	}
 
-	fmt.Printf("Host: %s, Status: %s, Endpoints: %+v", info.Host, info.Status, info.Endpoints)
+	//fmt.Printf("Host: %s, Status: %s, Endpoints: %+v", info.Host, info.Status, info.Endpoints)
 
+	Host := info.Host
+	Status := info.Status
+
+	myData := Server{
+		Host:   Host,
+		Status: Status,
+	}
+
+	prettyJSON, err := json.MarshalIndent(myData, "", "    ")
+	if err != nil {
+		log.Fatal("Failed to generate json", err)
+	}
+
+	fmt.Fprintf(ctx, "%s\n", string(prettyJSON))
 }
