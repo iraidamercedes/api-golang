d01bf3b4ad7650e96c052f42226413738c9ac09d
iraida07 <iraidabarreto07@hotmail.com>
2020-06-18 01:44:56 -0500

    :zap: Adds nested json print to return

--- api/test.go
+++ api/test.go
@@ -10,28 +10,32 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
+type endpoint struct {
+	IpAddress string
+	Grade     string
+}
+
+type Server struct {
+	Host      string
+	Status    string
+	Endpoints []endpoint
+}
+
 func requestURL(ctx *fasthttp.RequestCtx) {
 
 	req, _ := http.NewRequest("GET", "https://api.ssllabs.com/api/v3/analyze?host=sibcolombia.net", nil)
 
 	client := &http.Client{}
 	resp, _ := client.Do(req)
-
 	servers, _ := ioutil.ReadAll(resp.Body)
 	//println(string(servers))
-
 	fmt.Fprintf(ctx, "%s", string(servers))
 
-	type Server struct {
-		Host   string
-		Status string
-	}
-
 	var info Server
-
 	if err := json.Unmarshal([]byte(servers), &info); err != nil {
 		log.Fatal("Failed to generate json", err)
 	}
 
-	fmt.Printf("Host: %s, Status: %s", info.Host, info.Status)
+	fmt.Printf("Host: %s, Status: %s, Endpoints: %+v", info.Host, info.Status, info.Endpoints)
+
 }
